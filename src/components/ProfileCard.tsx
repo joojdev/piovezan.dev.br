404d48fde5708b526c52ae81f5b4ec2ad79e44f3
gcontiero11 <gustcontiero@gmail.com>
2025-08-08 10:56:55 -0300

    Refactor

--- src/components/ProfileCard.tsx
+++ src/components/ProfileCard.tsx
@@ -11,21 +11,15 @@ export default function ProfileCard() {
         draggable={false}
       />
       <div className="content">
-        <h1>
-          João Vitor Piovezan
-        </h1>
+        <h1>João Vitor Piovezan</h1>
         <div className="titles">
           <p>
-            <a
-              href="https://portais.ifsp.edu.br/scl/index.php/cursos/62-ensino/1762-bacharelado-em-engenharia-de-software.html"
-            >
+            <a href="https://portais.ifsp.edu.br/scl/index.php/cursos/62-ensino/1762-bacharelado-em-engenharia-de-software.html">
               Bacharelando em Engenharia de Software↗
             </a>
           </p>
           <p>
-            <a
-              href="https://portais.ifsp.edu.br/scl/index.php/cursos/62-ensino/102-curso-tecnico-em-informatica-para-internet-integrado-ao-ensino-medio.html"
-            >
+            <a href="https://portais.ifsp.edu.br/scl/index.php/cursos/62-ensino/102-curso-tecnico-em-informatica-para-internet-integrado-ao-ensino-medio.html">
               Técnico em Informática para Internet↗
             </a>
           </p>
