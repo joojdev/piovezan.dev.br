e3c0f807a5650b0188e1b8545e0225207fc8adf1
joojdev <jvpiovezan2905@gmail.com>
2025-12-01 22:29:45 -0300

    Fix: 1 second to work the clock

--- src/components/Clock.tsx
+++ src/components/Clock.tsx
@@ -4,20 +4,24 @@ import './Clock.css'
 export default function Clock() {
   const [clockText, setClockText] = useState('')
 
+  function updateClockText() {
+    const date = new Date()
+
+    const hours = date.getHours()
+    const minutes = date.getMinutes()
+
+    const displayHours = hours % 12 || 12
+    const isPM = hours >= 12
+
+    setClockText(
+      `${String(displayHours).padStart(2, '0')}:${String(minutes).padStart(2, '0')} ${isPM ? 'PM' : 'AM'}`,
+    )
+  }
+
   useEffect(() => {
-    const interval = setInterval(() => {
-      const date = new Date()
+    updateClockText()
 
-      const hours = date.getHours()
-      const minutes = date.getMinutes()
-
-      const displayHours = hours % 12 || 12
-      const isPM = hours >= 12
-
-      setClockText(
-        `${String(displayHours).padStart(2, '0')}:${String(minutes).padStart(2, '0')} ${isPM ? 'PM' : 'AM'}`,
-      )
-    }, 1000)
+    const interval = setInterval(updateClockText, 1000)
 
     return () => clearInterval(interval)
   }, [])
